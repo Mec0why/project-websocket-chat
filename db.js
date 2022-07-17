32c1c01d0d71a306df908764109ce6f33ec889d0
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-07-17 10:00:15 +0200

    Update db

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
 const db = {
-  messages: [
-    { author: 'John Doe', content: 'Hi!' },
-    {
-      author: 'John Doe',
-      text: 'It is nice to meet You!',
-    },
-    { author: 'You', content: 'Hi!' },
-    { author: 'You', content: 'How are You doing?' },
-  ],
+  messages: [],
+  users: [],
 };
 
 module.exports = db;
